13845197003f9003d686181b485a500bdf75604e
Javi <zenkiu@Javiers-MBP.fios-router.home>
2019-10-09 23:20:12 -0400

    update file name

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular.js'
 import FanceyScroller from './components/FancyScroller.js'
-import Viewpager from './components/Viewpager.js'
+import Viewpager from './components/ViewPager.js'
 
 class App extends React.Component {
   render() {
